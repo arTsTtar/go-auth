71b303ed233e2811e59be5013c27d835cfa452c1
Artūras <arturassisakovas@gmail.com>
2021-10-21 21:29:32 +0300

    added not null constrains to users struct

--- models/users.go
+++ models/users.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	Id       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"`
+	Name     string `json:"name" gorm:"notnull"`
+	Email    string `json:"email" gorm:"unique;notnull"`
+	Password []byte `json:"-" gorm:"notnull"`
 }
